c80d13ff43b81571129b6d8fa98565d030d48caf
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2021-02-07 10:27:47 -0500

    Still getting Errors, but rotating on right stick up/down now.

--- GnomeSpace/Assets/Scripts/CameraRotation.cs
+++ GnomeSpace/Assets/Scripts/CameraRotation.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd02fbdcf423344352cde2aa36247734d881585ccd046372e29313fa1e889113
-size 3007
+oid sha256:83ca65f71be4286d851e97002bed91af3f7d6db77ab9d4ab1c21e89d230d10a1
+size 2852
--- GnomeSpace/Assets/Scripts/SpaceGnome_02_InputActions.cs
+++ GnomeSpace/Assets/Scripts/SpaceGnome_02_InputActions.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f952f1a5ebb0c8a9c3f82680308aa5e8c640cc9f99eca12baacd7d603335c2a8
-size 21683
+oid sha256:263d5e35e29287896b7b7e9e01ee87c2c3201f04e0839a7bfdb9a7c24efd8674
+size 26192
