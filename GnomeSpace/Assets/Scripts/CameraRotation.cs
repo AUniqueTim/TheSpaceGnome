c3f92b466c85982ba041268844400d3eb1770634
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2021-02-08 12:59:53 -0500

    Player/Camera Rotation still needs work.

--- GnomeSpace/Assets/Scripts/CameraRotation.cs
+++ GnomeSpace/Assets/Scripts/CameraRotation.cs
@@ -13,10 +13,10 @@ public class CameraRotation : MonoBehaviour
     public Vector3 camRot;
     //Vector3 camXRot;
 
-    [SerializeField] private float maxXRot;
-    [SerializeField] private float maxYRot;
+    //[SerializeField] private float maxXRot;
+   // [SerializeField] private float maxYRot;
     [SerializeField] private float playerRotationSpeed;
-
+    [SerializeField] private float camRotationSpeed;
     public float camSpeed;
     
     public GameObject player;
@@ -27,12 +27,12 @@ public class CameraRotation : MonoBehaviour
     {
         camControls = new SpaceGnome_02_InputActions();
 
-        camControls.Camera.RotateCamera.performed += ctx => camRot = ctx.ReadValue<Vector3>();
+        camControls.Camera.RotateCamera.performed += ctx => camRot = ctx.ReadValue<Vector2>();
         camControls.Camera.RotateCamera.canceled += ctx => camRot = Vector2.zero;
         camControls.Camera.Rotate.performed += ctx => playerRot = ctx.ReadValue<Vector2>();
         camControls.Camera.Rotate.canceled += ctx => playerRot = Vector2.zero;
-        //camControls.Camera.RotateCameraOnXAxis.performed += ctx => camXRot = Vector2.up;
-      //  camControls.Camera.RotateCameraOnXAxis.canceled += ctx => camXRot = Vector2.zero;
+      //  camControls.Camera.RotateCameraOnXAxis.performed += ctx => camYRot = ctx.ReadValue<Vector2>();
+      //  camControls.Camera.RotateCameraOnXAxis.canceled += ctx => camYRot = Vector2.zero;
     }
     //public void RotateOnX()
     //{
@@ -40,29 +40,32 @@ public class CameraRotation : MonoBehaviour
     //}
     private void Update()
     {
-        //Vector3 camRotation = new Vector3(camRot.y, 0, camRot.z);
-        //ansform.Rotate(camRotation * camSpeed * Time.deltaTime, Space.Self);
-
-
+        //Vector3 camRotation = new Vector3(camRot.y, camRot.x, camRot.z);
+      //  transform.Rotate(camRotation * camSpeed * camRotationSpeed * Time.deltaTime, Space.World);
+        
+       // transform.Rotate(camRotation * camSpeed * Time.deltaTime, Space.Self);
+       //transform.Rotate(camRotation * camSpeed * Time.deltaTime, Space.Self);
         Vector3 playerRotation = new Vector3(playerRot.y, playerRot.x, playerRot.z);
         //if (camControls.Camera.Rotate.triggered) { player.transform.Rotate(playerRotation * playerRotationSpeed * camSpeed * Time.deltaTime, Space.Self); }
-       player.transform.Rotate(playerRotation * playerRotationSpeed * camSpeed * Time.deltaTime, Space.Self);
+       player.transform.Rotate(playerRotation * playerRotationSpeed * camSpeed * Time.deltaTime, Space.World);
+        //player.transform.Translate(playerRotation * camRotationSpeed * camSpeed * Time.deltaTime, Space.World);
+        //transform.Rotate(playerRotation * playerRotationSpeed * camSpeed * Time.deltaTime, Space.World);
         //transform.Rotate(playerRotation * camSpeed * Time.deltaTime, Space.Self);
-        //  Vector3 cameraXRotation = new Vector3(camYRot.x, camYRot.y);
+        // Vector3 cameraYRotation = new Vector3(camYRot.x, camYRot.y);
         //transform.Rotate(cameraXRotation * camSpeed * Time.deltaTime);
         //        transform.Rotate(cameraXRotation + transform.position * camSpeed * Time.deltaTime, Space.Self);
         //      if (camControls.Camera.RotateCameraOnXAxis.triggered) {  }
-        //  transform.RotateAround(player.transform.position, player.transform.position, cameraXRotation.x);
+        //  transform.RotateAround(player.transform.position, cameraYRotation, cameraYRotation.y);
 
         // player.transform.Rotate(cameraXRotation * camSpeed * Time.deltaTime, Space.Self);
 
-        //if (playerRotation.x >= maxXRot)
+        //if (camRotation.x >= maxXRot)
         //{
-        //    playerRotation.x = maxXRot;
+        //    camRotation.x = maxXRot;
         //}
-        //if (playerRotation.y >= maxYRot)
+        //if (camRotation.y >= maxYRot)
         //{
-        //    playerRotation.y = maxYRot;
+        //    camRotation.y = maxYRot;
         //}
 
     }
