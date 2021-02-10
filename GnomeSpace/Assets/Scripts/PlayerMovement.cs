7d2893837cf0ce17fe8742e7e3552dafb2d04b6b
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2021-02-10 18:41:00 -0500

    Added background. Player Rotation okay, but needs to be clamped, or else camera rotation does, and still don't have camera rotating around player.

--- GnomeSpace/Assets/Scripts/PlayerMovement.cs
+++ GnomeSpace/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea02db0d288486056b8e08d680b3d15986ae16aec174120eab25ba723a7594cd
-size 9699
+oid sha256:60bbe1686a26e5f3367567e9ad60d6f2ea7f546fd626fb02abcb87b07d05461d
+size 9849
--- GnomeSpace/Assets/Scripts/SpaceGnome_02_InputActions.cs
+++ GnomeSpace/Assets/Scripts/SpaceGnome_02_InputActions.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7087e9dd4d0d0e69b32751a4e18c147fc314b72b52bdc5444196998f4a34b4f0
-size 32781
+oid sha256:63463becebeb4ca29dbbdae2dad07c7a300cbb1b8c322333364f9f37620876e0
+size 32962
