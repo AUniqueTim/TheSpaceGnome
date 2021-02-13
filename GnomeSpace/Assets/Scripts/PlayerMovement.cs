2a732a264c7805174303b9fcb2d56de57e18b5c6
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2021-02-12 21:42:09 -0500

    no message

--- GnomeSpace/Assets/Scripts/PlayerMovement.cs
+++ GnomeSpace/Assets/Scripts/PlayerMovement.cs
@@ -122,8 +122,8 @@ public class PlayerMovement : MonoBehaviour
         if (jumpingAllowed && controls.Player.Jump.triggered && isJumping == false)
         {
 
-            playerRB.MovePosition(jump * -gravity * jumpSpeed * Time.time);
-            //transform.Translate(jump, Space.World);
+           playerRB.MovePosition(jump * -gravity * jumpSpeed * Time.time);
+           transform.Translate(jump, Space.Self);
             isJumping = true;
         }
         else { isJumping = false; }
