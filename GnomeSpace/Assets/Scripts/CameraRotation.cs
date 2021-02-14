46cb829bb50170d528d709231696aba87eef936e
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2021-02-14 18:11:26 -0500

    Getting closer to Camera rotation around character when idle. Camera script currently disabled, doing all rotation in player movement.

--- GnomeSpace/Assets/Scripts/CameraRotation.cs
+++ GnomeSpace/Assets/Scripts/CameraRotation.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08e2b82e3004137e27aaa98facb7e2c4c9c038d81d06f816feb04a3af19b802d
-size 5495
+oid sha256:d15e086b7a103e0525934b80537bd60b6ba133e1fe12f97413b3f908de4c3e65
+size 5507
--- GnomeSpace/Assets/Scripts/PlayerMovement.cs
+++ GnomeSpace/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5444faa5f456b1c0de844b2f036a1392fac4b8992f78b53998d5441cb159f826
-size 10166
+oid sha256:fb311de9f82ba79eb9142b17d53dd44754363234099ae26e4b35f7fd6b1480e5
+size 12483
