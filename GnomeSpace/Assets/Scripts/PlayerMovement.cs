21d809848ab4a4241d95aba254c4a546aba05510
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2021-02-15 17:16:57 -0500

    Made new Test Scene Gym added some audio on coin pick up and asteroid collision.

--- GnomeSpace/Assets/Scripts/PlayerMovement.cs
+++ GnomeSpace/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:037e0d83c67f92c0bc87d349d6ec9f7885ffeb3f872140a67ae5b50ccfb94c06
-size 10848
+oid sha256:f272289c590f1def8e8972f8b279007e0e3604a55240ff11e8cf79cc445266ab
+size 10943
