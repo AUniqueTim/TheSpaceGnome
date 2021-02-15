057fd9502caa5dd9fdb1b7a4afeaeededdca14c4
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2021-02-15 00:18:53 -0500

    FINALLY got camera close to where it needs to be, still needs tweaking.

--- GnomeSpace/Assets/Scripts/PlayerMovement.cs
+++ GnomeSpace/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb311de9f82ba79eb9142b17d53dd44754363234099ae26e4b35f7fd6b1480e5
-size 12483
+oid sha256:037e0d83c67f92c0bc87d349d6ec9f7885ffeb3f872140a67ae5b50ccfb94c06
+size 10848
--- GnomeSpace/Assets/Scripts/SpaceGnome_02_InputActions.cs
+++ GnomeSpace/Assets/Scripts/SpaceGnome_02_InputActions.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd978de657306456eb8c835224f1c2b9ee67aa81ec75d73f179db8756a3d5c30
-size 32897
+oid sha256:b91f3c674725da3d7b7e605750de40187b9bb612c61fd30dd571f6568c6ff689
+size 32633
