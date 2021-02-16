a8d52a0256dda7b55a0a13c9b52a229f8a262581
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2021-02-16 15:45:15 -0500

    Working on Animations/Gnome Controller

--- GnomeSpace/Assets/Scripts/CameraControls.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:22722bbe534fc66f91474a3f5d138212a73cdc571f56e78e29bc54ec2a6ec36d
-size 4583
--- GnomeSpace/Assets/Scripts/PlayerMovement.cs
+++ GnomeSpace/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f272289c590f1def8e8972f8b279007e0e3604a55240ff11e8cf79cc445266ab
-size 10943
+oid sha256:7514b00c46be3506a03604afc83364668ebad6af394b0674be9e6e082b1e366f
+size 9284
