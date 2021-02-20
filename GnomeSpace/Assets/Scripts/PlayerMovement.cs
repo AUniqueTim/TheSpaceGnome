d5af0de879f6a925a7f5ffcc67892875197537ff
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2021-02-20 18:43:46 -0500

    Boost Bar working properly.

--- GnomeSpace/Assets/Scripts/PlayerMovement.cs
+++ GnomeSpace/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9060c64170772b5a12ff1d3ce9fc00d01aecac11928c20719604cf284b5ba348
-size 14907
+oid sha256:a85495f92cb9eeaedc1e0677161f1d82dd4d0115707e75635c2fdabf81c6c8dc
+size 14981
