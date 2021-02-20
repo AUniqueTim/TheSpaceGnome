5da4ca2e121c37fe110bfce68975a8f127bef236
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2021-02-20 14:57:40 -0500

    Close to done for Brackey's build.  Platform Spawning works well but slows down game a lot, makes it too easy, need to make it a limited resource.

--- GnomeSpace/Assets/Scripts/ObjectInstanstiator.cs
+++ GnomeSpace/Assets/Scripts/ObjectInstanstiator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f2686789605e9e463010405bc1df3a94501786769cfcb812db8f35cfaae164b
-size 3887
+oid sha256:c4c7e8e95fcc126b78ca98aa10a0d1555d5dc6d0b904ff9ef664f8481d604000
+size 3878
--- GnomeSpace/Assets/Scripts/PlayerMovement.cs
+++ GnomeSpace/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc3df99f47c49b7da5a5624714fc82e197f6c50477489a97731452d3c14cad8c
-size 11882
+oid sha256:a42f7be4ecf5fb23c64f0c42dddd3bb692ce34a2695e028d0b8684d828fd54fc
+size 14878
