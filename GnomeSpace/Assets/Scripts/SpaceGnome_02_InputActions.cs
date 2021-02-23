beb37a4e66d43d5e4f2e800f8c27fbe1886b7e0f
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2021-02-23 18:41:39 -0500

    HDRP borked, starting new branch based on last working commit before trying to upgrade

--- GnomeSpace/Assets/Scripts/SpaceGnome_02_InputActions.cs
+++ GnomeSpace/Assets/Scripts/SpaceGnome_02_InputActions.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcb709518e890dc8938a43d49656554257d8b677db8ae2d0af539b50d06b464b
-size 44460
+oid sha256:826137beaa1bdd5abb79778c33f2ad39d1c0b9ceebde371f562ae9c53244092c
+size 46395
