391a81ba31c322397439494afc93e661eedd1554
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2021-02-26 14:43:16 -0500

    Fixed some bugs, put time ratio of Rotation Engine in Fixed Update, seems to have fixed perf. problems.

--- GnomeSpace/Assets/Scripts/PlayerMovement.cs
+++ GnomeSpace/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85b674dba8826ce38a628f4822fb0b0dacec0bc0973bfe69246de4fcbde9cf13
-size 20289
+oid sha256:858ef2e78407dad229cc4d45e9c67999de113851e7986b5c8b398e7afb190ad6
+size 19945
