ccc713c33001859057edaf7f321a45da133e378a
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2021-02-26 12:33:15 -0500

    Messing with Cinemachine,t rying to get post processing to work so I can auto focus on gnome.

--- GnomeSpace/Assets/Scripts/PlayerMovement.cs
+++ GnomeSpace/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:177e10f7217d132afb46a85732e00368753a0ad9eab4aebc6f7a8929d3fab39e
-size 20294
+oid sha256:85b674dba8826ce38a628f4822fb0b0dacec0bc0973bfe69246de4fcbde9cf13
+size 20289
