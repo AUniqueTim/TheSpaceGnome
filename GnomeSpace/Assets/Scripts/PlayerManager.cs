f903bfb31002830cf24f78b134bf12125e9ec390
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2021-02-27 16:50:57 -0500

    Added more satellite models and got them mostly working with new rotation object but performance isn't great.
    Former-commit-id: 6802ed35667abc8b2ae98c1d35164b97d8204d9a

--- GnomeSpace/Assets/Scripts/PlayerManager.cs
+++ GnomeSpace/Assets/Scripts/PlayerManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:373f9b8d7833482bb8d354ba37610232accd66460fe69949a6c596305290efa2
-size 6433
+oid sha256:aa63fa06876760a41acc0ef1c4bff35e391e5bcad2722f166c41bc669f9f1220
+size 6471
--- GnomeSpace/Assets/Scripts/SpaceGnome_02_InputActions.cs
+++ GnomeSpace/Assets/Scripts/SpaceGnome_02_InputActions.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b59e07ce39c51b54d28baa5b78540fafd3b60aa35f6a9cb253089792da4a321
-size 59622
+oid sha256:077d9fc7938d231ce397f3e6827f295a024c562f689809db0bf66b935cf534e2
+size 59650
